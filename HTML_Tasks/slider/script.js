a164e91d7cb2c3d8755d171a7265a2ca74ab6431
Kartofechka <126353546+Kartofechka@users.noreply.github.com>
2025-10-16 13:21:00 +0300

    Add files via upload

--- HTML_Tasks/slider/script.js
+++ HTML_Tasks/slider/script.js
@@ -6,8 +6,35 @@ const img_src = ['assets/1.jpg', 'assets/2.jpg', 'assets/3.jpg', 'assets/4.jpg']
 let index = 0;
 image.src = img_src[index];
 
+const img_info = ['Шрексель', 'Окак', 'Чиловый парень', 'Выхожу после экзамена по дискретке'];
+const p_info = document.getElementById('p_info')
+const info_btn = document.getElementById('info')
+
+const random_btn = document.getElementById('random')
+let random_condition = -1
+
+let last_index = index
+
 function updateImage() {
-    image.src = img_src[index];
+    p_info.textContent = ''
+    if (random_condition != -1){
+        random_index = Math.floor(Math.random() * img_src.length)
+        if (random_index === last_index) {
+            image.src = img_src[random_index + 1]
+            setCanvasBackground(img_src[random_index + 1]);
+            last_index = random_index + 1
+        }
+        else {
+            image.src = img_src[random_index]
+            setCanvasBackground(img_src[random_index]);
+            last_index = random_index
+        }
+    }
+    else {
+        image.src = img_src[index];
+        setCanvasBackground(img_src[index]);
+        last_index = index
+    }
 }
 
 function right() {
@@ -26,7 +53,70 @@ function left() {
     updateImage();
 }
 
+function show_info(){
+    p_info.textContent = img_info[index]
+}
+
+function change_random(){
+    random_condition *= -1
+}
+
 button_left.addEventListener('click', left);
 button_right.addEventListener('click', right);
+info_btn.addEventListener('click', show_info)
+random_btn.addEventListener('click', change_random)
 
-updateButtons();
+
+
+
+
+const canvas = document.getElementById('drawing_picture');
+const ctx = canvas.getContext('2d');
+const pos = { x: 0, y: 0 };
+const clear_btn = document.getElementById('clear')
+
+canvas.addEventListener('mousedown', setPosition);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseenter', setPosition);
+
+
+function setPosition(e) {
+    pos.x = e.clientX - canvas.getBoundingClientRect().left;
+    pos.y = e.clientY - canvas.getBoundingClientRect().top;
+}
+
+function draw(e) {
+  if (e.buttons !== 1) return;
+
+  const user_color = document.getElementById('drawer_color').value;
+  const user_width = document.getElementById('drawer_width').value;
+
+    ctx.beginPath();
+    changeWidtg(user_width);
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = user_color;
+    ctx.moveTo(pos.x, pos.y);
+    setPosition(e);
+    ctx.lineTo(pos.x, pos.y);
+    ctx.stroke();
+}
+
+function clearPlace(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function changeWidtg(user_width){
+    ctx.lineWidth = user_width;
+}
+
+function setCanvasBackground(src) {
+    const bg = new Image();
+    bg.src = src;
+    bg.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // очистить холст
+        ctx.drawImage(bg, 0, 0, canvas.width, canvas.height); // нарисовать фон
+    };
+}
+
+
+clear_btn.addEventListener('click', clearPlace)
