bba166881465a1644f99c783df3ca342c5a149a6
Kartofechka <126353546+Kartofechka@users.noreply.github.com>
2025-11-12 11:56:56 +0300

    Add files via upload

--- HTML_Tasks/pomedoro_time/script.js
+++ HTML_Tasks/pomedoro_time/script.js
@@ -1,40 +1,66 @@
 const pomodoro = $('#pomodoro-button')
-console.log(pomodoro)
-const lb = $('lb-button')
-console.log(lb)
+const lb = $('#lb-button')
+const sb = $('#sb-button')
 const timer = $('#time-show')
 const start = $('#start-button')
 const pause = $('#pause-button')
 const reset = $('#reset-button')
 
-let time = 10
+let time = 600
 let interval
 let mins
 let seconds
 
-function show_time(){
-    if (time != 0){
-    mins = Math.floor(time / 60)
-    seconds = time - (mins * 60)
-    timer.text(`${mins} минут,  ${seconds} секунд`);
-    }
-    else {
+function show_time(time){
+    if (time > 0){
+        mins = Math.floor(time / 60)
+        seconds = time % 60
+        timer.text(`${mins} мин., ${seconds} сек.`)
+    } else {
         timer.text(`Время вышло`)
         clearInterval(interval)
     }
 }
 
-start.click(function(){
+function start_timer(){
+    clearInterval(interval)
     interval = setInterval(function(){
-        time--;
-        show_time(time);
-    }, 1000);
-} )
+        time--
+        show_time(time)
+    }, 1000)
+}
+
+start.click(function(){
+    start_timer()
+})
 
 pause.click(function(){
     clearInterval(interval)
 })
 
 reset.click(function(){
+    clearInterval(interval)
+    time = 600
+    show_time(time)
+})
+
+pomodoro.click(function(){
+    clearInterval(interval)
     time = 1500
+    show_time(time)
+    start_timer()
+})
+
+lb.click(function(){
+    clearInterval(interval)
+    time = 900
+    show_time(time)
+    start_timer()
+})
+
+sb.click(function(){
+    clearInterval(interval)
+    time = 300
+    show_time(time)
+    start_timer()
 })
