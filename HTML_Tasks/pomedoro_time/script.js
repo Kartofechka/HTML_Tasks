3f38b042be9963f38ddbaf39a41ec0a7e3882ef9
Kartofechka <126353546+Kartofechka@users.noreply.github.com>
2025-11-14 11:53:07 +0300

    El tomate complete

--- HTML_Tasks/pomedoro_time/script.js
+++ HTML_Tasks/pomedoro_time/script.js
@@ -5,11 +5,14 @@ const timer = $('#time-show')
 const start = $('#start-button')
 const pause = $('#pause-button')
 const reset = $('#reset-button')
+const skip = $('#skip-button')
 
-let time = 600
+let time = 1500
 let interval
 let mins
 let seconds
+let currentMode = "pomodoro"
+
 
 function show_time(time){
     if (time > 0){
@@ -19,8 +22,10 @@ function show_time(time){
     } else {
         timer.text(`Время вышло`)
         clearInterval(interval)
+        nextMode()
     }
 }
+
 
 function start_timer(){
     clearInterval(interval)
@@ -28,6 +33,33 @@ function start_timer(){
         time--
         show_time(time)
     }, 1000)
+}
+
+
+function setMode(mode){
+    clearInterval(interval)
+    currentMode = mode
+    if(mode === "pomodoro"){
+        time = 1500
+        $('body').css('background', '#a8011aff')
+    } else if(mode === "longbreak"){
+        time = 900
+        $('body').css('background', '#007d0aff')
+    } else if(mode === "shortbreak"){
+        time = 300
+        $('body').css('background', '#007d43ff')
+    }
+    show_time(time)
+    start_timer()
+}
+
+
+function nextMode(){
+    if(currentMode === "pomodoro"){
+        setMode("longbreak")
+    } else {
+        setMode("pomodoro")
+    }
 }
 
 start.click(function(){
@@ -39,28 +71,49 @@ pause.click(function(){
 })
 
 reset.click(function(){
-    clearInterval(interval)
-    time = 600
+    clearInterval(interval);
+    time = 1500;
     show_time(time)
 })
 
 pomodoro.click(function(){
-    clearInterval(interval)
-    time = 1500
-    show_time(time)
-    start_timer()
+    setMode("pomodoro")
 })
 
 lb.click(function(){
-    clearInterval(interval)
-    time = 900
-    show_time(time)
-    start_timer()
+    setMode("longbreak")
 })
 
 sb.click(function(){
-    clearInterval(interval)
-    time = 300
-    show_time(time)
-    start_timer()
+    setMode("shortbreak")
+})
+
+skip.click(function(){
+    nextMode()
+})
+
+
+
+
+const todoInput = $('#todo-input')
+const addTodo = $('#add-todo')
+const todoList = $('#todo-list')
+
+addTodo.click(function(){
+    const task = todoInput.val().trim()
+    if(task !== ""){
+        const li = $('<li></li>').text(task)
+        const delBtn = $('<button>✖</button>').css({
+            marginLeft: '10px',
+            background: 'red',
+            color: 'white',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer'
+        })
+        delBtn.click(function(){ li.remove() })
+        li.append(delBtn)
+        todoList.append(li)
+        todoInput.val("")
+    }
 })
